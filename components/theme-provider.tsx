3e82ba91f01237e175829f61a87d1e68a393f7b4
Azis Alvriyanto <noreply@membasuh.com>
2026-06-02 09:03:18 +0700

    fix(theme): add __name shim for runtime compatibility

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,11 +1,22 @@
-"use client";
+'use client'
 
-import * as React from "react";
-import { ThemeProvider as NextThemesProvider } from "next-themes";
+import * as React from 'react'
+import { ThemeProvider as NextThemesProvider } from 'next-themes'
+
+const themeScript = `
+  if (typeof __name === 'undefined') {
+    window.__name = (fn, name) => fn;
+  }
+`
 
 export function ThemeProvider({
   children,
   ...props
 }: React.ComponentProps<typeof NextThemesProvider>) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+  return (
+    <>
+      <script dangerouslySetInnerHTML={{ __html: themeScript }} />
+      <NextThemesProvider {...props}>{children}</NextThemesProvider>
+    </>
+  )
 }
